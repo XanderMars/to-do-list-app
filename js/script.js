68a723973c123ea32df2d2c68f382a07fcb6efbd
XanderMars <98784017+XanderMars@users.noreply.github.com>
2022-06-16 15:17:28 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,2 +1,10 @@
 let inputs = $('input');
 inputs.addClass('my-class');
+
+let list = $('#list');
+  list.append("<li>the value of the (inputValue) from the input element<li>");
+let li = $('<li></li>');
+  list.append(li);
+
+let inputValue = $('#input').val();
+  li.append(inputValue);
